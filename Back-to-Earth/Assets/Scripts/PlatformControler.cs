09d4b2dac9ab9e7e6dcaf5092ef2498f8dd77363
Nico Begic <nicolasbegic@alice.de>
2015-07-26 15:31:21 +0200

    Created PlatformStruct
    Added PlatformController|Added Platform-Scripts

---
+++ Back-to-Earth/Assets/Scripts/PlatformControler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformControler : MonoBehaviour
+{
+    private Platform platformType;
+
+    public Platform PlatformType 
+    {
+        get { return platformType; }
+        set { platformType = value; } 
+    }
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
