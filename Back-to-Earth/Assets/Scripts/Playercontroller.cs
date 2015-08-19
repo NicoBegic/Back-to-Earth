0e647809ba99de69d629baf296b7704f0fe2b20b
Nico_Begic <nicolasbegic@alice.de>
2015-08-19 17:23:55 +0200

    [NEW] TempPlayer
    added new tempplayer for chunkloading

---
+++ Back-to-Earth/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playercontroller : MonoBehaviour 
+{
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));
+        }
+	}
+}
